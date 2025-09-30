d47d0d7c2ff45e9535da8fb8193cb43c4082bade
“lichengcheng” <li_chenchen77@163.com>
2025-09-30 16:43:23 +0800

    first commit

---
+++ src/views/home/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="relative w-full overflow-hidden rounded-lg bg-black/[0.96]">
+    <div
+      class="absolute flex w-full flex-col items-center justify-center gap-2 p-8 text-center font-heading text-white"
+    >
+      <span class="text-4xl font-semibold"> 辰辰 </span>
+      <span class="font-sans font-light">Build spline animations with style.</span>
+      <audio :href="musicSrc" :src="musicSrc" autoplay controls></audio>
+    </div>
+    <div class="flex">
+      <Spline :scene="sceneUrl" class="mt-24 size-full" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Spline from './components/Spline.vue'
+
+const sceneUrl = 'https://prod.spline.design/kZDDjO5HuC9GJUM2/scene.splinecode'
+const musicSrc = ref('./blog-bg.mp3')
+</script>
+
+<style scoped>
+.size-full {
+  width: 100%;
+  height: calc(100vh - 6rem);
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools(), tailwindcss()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true, // 如需使用可计算样式（如 darken、lighten 等），建议开启 :cite[1]:cite[3]
+        // 在这里定义全局 Less 变量
+        modifyVars: {
+          'primary-color': '#1DA57A', // 例如，覆盖 Ant Design 的主色 :cite[1]
+          'link-color': '#1DA57A',
+          'border-radius-base': '2px',
+        },
+        // 或者，通过 additionalData 引入全局文件
+        // additionalData: `@import '@/assets/styles/variables.less';` :cite[3]
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
